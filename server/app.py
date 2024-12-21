6d5ff027c2eea9d4b61513435537c2d4b5166c49
nicolaaggeri <nicola.aggeri@edu.itspiemonte.it>
2024-12-21 18:25:28 +0100

    Update app.py

--- server/app.py
+++ server/app.py
@@ -16,8 +16,6 @@ from mysql.connector import Error
 ###############################################################################
 #                           CONFIGURAZIONE LOGGING                            #
 ###############################################################################
-import logging
-import os
 
 def configure_logging():
     """
@@ -47,16 +45,16 @@ def configure_logging():
 
     return periodic_logger
 
+periodic_logger = configure_logging()
+
 ###############################################################################
 #                          CONFIGURAZIONE PYDANTIC                            #
 ###############################################################################
 
-import os
 from datetime import datetime, timedelta
 from typing import List, Optional
 from pydantic import BaseModel, ValidationError, validator
 from datetime import datetime
-from timestamp_utils import validate_timestamp  # Se preferisci spostare la validazione timestamp in un file separato
 
 # Mappatura campi -> id anomalia
 FIELD_TO_ANOMALIA_ID = {
